9c35be3ec6ad9a790ed41bb491bcf53db3354bb3
Jayden <n10217011@qut.edu.au>
2024-09-11 01:33:24 +1000

    Student class created

--- src/main/java/org/codecrafters/educa/App.java
+++ src/main/java/org/codecrafters/educa/App.java
@@ -1,11 +1,12 @@
 package org.codecrafters.educa;
 
-import org.codecrafters.educa.db.DatabaseConnection;
+import org.codecrafters.educa.db.*;
+import org.codecrafters.educa.profiles.*;
 
 import java.sql.Connection;
 
 public class App {
     public static void main(String[] args) {
         Connection connection = DatabaseConnection.getInstance();
+        }
     }
-}
---
+++ src/main/java/org/codecrafters/educa/profiles/student.java
@@ -0,0 +1,62 @@
+package org.codecrafters.educa.profiles;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.util.Date;
+
+public class student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String DOB;
+    private String conditions;
+
+    public student(int id, String firstName, String lastName, int age, String DOB, String conditions){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.DOB = DOB;
+        this.age = Period.between(LocalDate.parse(DOB), LocalDate.now()).getYears();
+        this.conditions = conditions;
+    }
+
+    public student(String firstName, String lastName, int age, String DOB, String conditions){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.DOB = DOB;
+        this.age = Period.between(LocalDate.parse(DOB), LocalDate.now()).getYears();
+        this.conditions = conditions;
+    }
+
+    public int getId() { return id;}
+
+    public String getFirstName() {return firstName;}
+
+    public String getLastName() {return lastName;}
+
+    public String getDOB() {return DOB;}
+
+    public int getAge() {return age;}
+
+    public String getConditions() {return conditions;}
+
+    public void setFirstName(String firstName){ this.firstName = firstName;}
+
+    public void setLastName(String lastName){this.lastName = lastName;}
+
+    public void setDOB(String DOB){
+        this.DOB = DOB;
+        this.age = Period.between(LocalDate.parse(DOB), LocalDate.now()).getYears();
+    }
+
+    public void setConditions(String conditions){this.conditions = conditions};
+
+    public String toString() {
+        return "ID: " + id +
+                ", Name: " + firstName + " " + lastName +
+                ", Date of Birth" + DOB + ", Age: " + age +
+                ", Conditions: " + conditions;
+    }
+}
