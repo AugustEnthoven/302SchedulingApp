ff05df1c1d0738b53d6593b70f7286c9005b53c4
Jayden <n10217011@qut.edu.au>
2024-09-16 00:31:14 +1000

    GUI start

--- src/main/java/org/codecrafters/educa/App.java
+++ src/main/java/org/codecrafters/educa/App.java
@@ -3,11 +3,23 @@ package org.codecrafters.educa;
 import org.codecrafters.educa.db.*;
 import org.codecrafters.educa.profiles.*;
 
-import java.sql.Connection;
-import java.util.List;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class App {
+import java.io.IOException;
+
+public class App extends Application{
+    @Override
+    public void start(Stage stage) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Test");
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main(String[] args) {
-
+        launch();
     }
 }
