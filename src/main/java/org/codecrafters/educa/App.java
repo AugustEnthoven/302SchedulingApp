aedfc9f3453d2fda2737b202cc1fd6b502c0abdc
dylan <bokitran@Bokis-MacBook-Air.local>
2024-09-17 12:59:46 +1000

    first attempt for opening window

--- src/main/java/org/codecrafters/educa/App.java
+++ src/main/java/org/codecrafters/educa/App.java
@@ -1,10 +1,34 @@
 package org.codecrafters.educa;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.codecrafters.educa.db.DatabaseConnection;
+import org.codecrafters.educa.models.user.UserDAO;
 
+import java.io.IOException;
 import java.sql.Connection;
 
-public class App {
+public class App extends Application {
+    private static SceneManager sceneManager;
+
+    private static UserDAO userDAO;
+
+    public static UserDAO getUserDAO() { return userDAO; }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        // Create sceneManger to control switching scene
+        sceneManager = new SceneManager(stage);
+        // Check if the stage is loaded correctly
+        Stage stage1 = sceneManager.getStage();
+        stage1.initStyle(StageStyle.UNDECORATED);
+
+        sceneManager.switchScene("home", "Home");
+        stage1.setTitle("Home");
+
+        stage1.show();
+    }
     public static void main(String[] args) {
         Connection connection = DatabaseConnection.getInstance();
     }
--- src/main/java/org/codecrafters/educa/SceneManager.java
+++ src/main/java/org/codecrafters/educa/SceneManager.java
@@ -1,2 +1,77 @@
-package org.codecrafters.educa;public class SceneManager {
+package org.codecrafters.educa;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Stack;
+
+public class SceneManager {
+    private final ObjectProperty<Stage> stage = new SimpleObjectProperty<>(null);
+    // Scenes collection
+    private final Map<String, Scene> scenes;
+    // Scenes stack
+    private final Stack<Scene> stack;
+    private final ObjectProperty<Scene> current = new SimpleObjectProperty<>(null);
+
+    public Stage getStage() { return stage.get(); }
+
+    public SceneManager(Stage thisStage){
+        thisStage.setResizable(false);
+        stage.set(thisStage);
+        this.scenes = new HashMap<>();
+        this.stack = new Stack<>();
+    }
+
+    public void switchScene(String sceneId, String title){
+        // Get scene from sceneId
+        Scene scene = scenes.get(sceneId);
+        // Set scene title
+        stage.get().setTitle(title);
+        stage.get().setResizable(false);
+
+        if (scene != null){
+            // Push old scene to the stack
+            stack.push(stage.get().getScene());
+            // Set new scene with current scene
+            stage.get().setScene(scene);
+            current.set(scene);
+        } else {
+            // Create new scene
+            try {
+                // Get file path of the FXML file from sceneId
+                String fxml = "pages/" + sceneId.toLowerCase() + ".fxml";
+                // Load CSS stylesheet for the scene
+                String css = Objects.requireNonNull(getClass().getResource("/styles.css")).toExternalForm();
+                // Create FXMLLoader to run FXML file
+                FXMLLoader loader = new FXMLLoader(App.class.getResource(fxml));
+                // Load the FXML file and create new scene
+                Scene newScene = new Scene(loader.load(), 1920, 1080);
+                // Add stylesheet for new scene
+                newScene.getStylesheets().add(css);
+                // Push the old scene to the stack
+                stack.push(stage.get().getScene());
+                // Store the new scene
+                scenes.put(sceneId, newScene);
+                // Set new scene as current scene
+                stage.get().setScene(newScene);
+                current.set(newScene);
+            } catch (IOException exception) {
+                System.out.println("Error loading scene: " + exception.getMessage());
+            }
+        }
+    }
+    public void back() {
+        if (!stack.isEmpty()) {
+            Scene prevScene = stack.pop();
+            stage.get().setScene(prevScene);
+        }
+    }
+    public Scene getCurrentScene() { return stage.get().getScene(); }
 }
