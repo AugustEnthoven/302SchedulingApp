e2ab0208bc7ee7c30cfd86c062ec034952f6a823
Anh Nguyen <anh.nguyen762005@gmail.com>
2024-09-18 13:49:18 +1000

    login and register pages implemented

--- src/main/java/org/codecrafters/educa/App.java
+++ src/main/java/org/codecrafters/educa/App.java
@@ -1,13 +1,17 @@
 package org.codecrafters.educa;
 
+import com.password4j.Password;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import org.codecrafters.educa.db.DatabaseConnection;
+import org.codecrafters.educa.models.authentication.Authentication;
 import org.codecrafters.educa.models.user.UserDAO;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 public class App extends Application {
     public static final int WIDTH = 600;
@@ -17,11 +21,15 @@ public class App extends Application {
 
     private static UserDAO userDAO;
 
+    private static Authentication authenticatedSession;
+
     public static UserDAO getUserDAO() { return userDAO; }
 
     public static Stage getStage() { return sceneManager.getStage(); }
 
     public static SceneManager getSceneManager() { return sceneManager; }
+
+    public static Authentication getAuthenticatedSession() { return authenticatedSession; }
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -29,12 +37,27 @@ public class App extends Application {
 
         Stage thisStage = sceneManager.getStage();
         thisStage.initStyle(StageStyle.UNDECORATED);
-        sceneManager.switchScene("studentProfile", "Student Profile");
-        thisStage.setTitle("Student Profile");
+
+        // Authenticate user -  if user is not logged in, show login scene
+        if (authenticatedSession.isAuthenticated()){
+            sceneManager.switchScene("home", "Home");
+            thisStage.setTitle("EduCalendar - Home");
+        } else {
+            sceneManager.switchScene("login", "Login");
+            thisStage.setTitle("EduCalendar - Login");
+        }
         thisStage.show();
     }
     public static void main(String[] args) {
-//        Connection connection = DatabaseConnection.getInstance();
+        // Initialisations
+        authenticatedSession = new Authentication();
+        // Create new user database
+        userDAO = new UserDAO();
+        userDAO.createUsersTable();
+        userDAO.addUser("Null", "teacher", "guest", Password.hash("N/A").withArgon2().getResult());
+        // Database connection
+        Connection connection = DatabaseConnection.getInstance();
+
         launch();
     }
 }
