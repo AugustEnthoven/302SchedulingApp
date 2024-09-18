438c16c09174804d7288ecc5dc121e2dd9c02fb4
Jayden <n10217011@qut.edu.au>
2024-09-18 11:57:56 +1000

    Added error handling to Student Creation - can only submit if all fields contain valid value (not null, date is parseable)

--- src/main/java/org/codecrafters/educa/App.java
+++ src/main/java/org/codecrafters/educa/App.java
@@ -15,7 +15,7 @@ public class App extends Application{
     public void start(Stage stage) throws IOException{
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Student-Field.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 480);
-        stage.setTitle("Test");
+        stage.setTitle("EduCA");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/org/codecrafters/educa/AppController.java
+++ src/main/java/org/codecrafters/educa/AppController.java
@@ -1,12 +1,18 @@
 package org.codecrafters.educa;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import org.codecrafters.educa.db.StudentDAO;
 import org.codecrafters.educa.profiles.Student;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class AppController {
+    @FXML
+    public Button createStudentButton;
     @FXML
     private TextField firstNameTextField;
     @FXML
@@ -22,15 +28,23 @@ public class AppController {
     }
 
     @FXML
+    protected void onStudentFieldEdit(){
+        boolean validDate;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        try {
+            LocalDate.parse(dobTextField.getText(), formatter);
+            validDate = true;
+        } catch(Exception e){validDate = false;}
+        createStudentButton.setDisable(!(!firstNameTextField.getText().isEmpty() & !lastNameTextField.getText().isEmpty() &
+                !dobTextField.getText().isEmpty() & !conditionsTextField.getText().isEmpty() & validDate));
+    }
+    @FXML
     protected void onCreateStudentClick(){
         String firstName = firstNameTextField.getText();
         String lastName = lastNameTextField.getText();
         String dob = dobTextField.getText();
         String conditions = conditionsTextField.getText();
-        if (!firstName.isEmpty() & !lastName.isEmpty() & !dob.isEmpty() & !conditions.isEmpty()){
-            studentDAO.insert(new Student(firstName, lastName, dob, conditions));
-        } else {
-            System.out.println("can't be null");
-        }
+        studentDAO.insert(new Student(firstName, lastName, dob, conditions));
+        System.out.println("Student Profile Created Successfully");
     }
 }
