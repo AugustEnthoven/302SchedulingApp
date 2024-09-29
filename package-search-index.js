718d0d6046f0e42eb3e674b7f84d8e1d6df04f43
JaydenWilson547 <106229529+JaydenWilson547@users.noreply.github.com>
2024-09-29 09:48:18 +0000

    Deploying to gh-pages from @ AugustEnthoven/302SchedulingApp@5b2854a2d9e9a1801643e87f003ed572ba396c6d 🚀

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.codecrafters.educa","l":"org.codecrafters.educa"},{"m":"org.codecrafters.educa","l":"org.codecrafters.educa.components"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.codecrafters.educa","l":"org.codecrafters.educa"},{"m":"org.codecrafters.educa","l":"org.codecrafters.educa.components"},{"m":"org.codecrafters.educa","l":"org.codecrafters.educa.profiles"}];updateSearchResults();
