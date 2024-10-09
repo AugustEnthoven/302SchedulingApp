aeacb90878c9c263b37a23efb565f493805959bb
AugustEnthoven <n11138947@qut.edu.au>
2024-10-09 18:22:40 +1000

    Updated the controllers for timetable and editpreference

--- src/main/java/org/codecrafters/educa/EditPreferenceController.java
+++ src/main/java/org/codecrafters/educa/EditPreferenceController.java
@@ -10,28 +10,44 @@ import java.util.ResourceBundle;
 public class EditPreferenceController implements Initializable {
 
     @FXML
-    private ChoiceBox<String> classDropDown;
+    private ChoiceBox<String> EnglishChoicebox;
 
     @FXML
-    private ChoiceBox<String> preferenceDropDown;
+    private ChoiceBox<String> HistoryChoicebox;
 
-    private String[] classes = {"English","History","Science","PE","Math","Spanish"};
+    @FXML
+    private ChoiceBox<String> SpanishChoicebox;
+
+    @FXML
+    private ChoiceBox<String> ScienceChoicebox;
+
+    @FXML
+    private ChoiceBox<String> PEChoicebox;
+
+    @FXML
+    private ChoiceBox<String> MathChoicebox;
 
     private String[] preferences = {"1","2","3","4","5"};
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
-        classDropDown.getItems().addAll(classes);
-        preferenceDropDown.getItems().addAll(preferences);
+        EnglishChoicebox.getItems().addAll(preferences);
+        HistoryChoicebox.getItems().addAll(preferences);
+        SpanishChoicebox.getItems().addAll(preferences);
+        ScienceChoicebox.getItems().addAll(preferences);
+        PEChoicebox.getItems().addAll(preferences);
+        MathChoicebox.getItems().addAll(preferences);
     }
 
-    public void onBackButtonClick()
+    @FXML
+    void onBackButtonClick()
     {
 
     }
 
-    public void onSubmitClick()
+    @FXML
+    void onSubmitClick()
     {
 
     }
--- src/main/java/org/codecrafters/educa/TimeTableController.java
+++ src/main/java/org/codecrafters/educa/TimeTableController.java
@@ -1,4 +1,30 @@
 package org.codecrafters.educa;
 
+import javafx.fxml.FXML;
+
 public class TimeTableController {
+
+    @FXML
+    void onEditButtonClick()
+    {
+
+    }
+
+    @FXML
+    void onDeleteButtonClick()
+    {
+
+    }
+
+    @FXML
+    void onNewButtonClick()
+    {
+
+    }
+
+    @FXML
+    void onCellClick()
+    {
+
+    }
 }
